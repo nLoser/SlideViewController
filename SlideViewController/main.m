8956a17f254caf5ae1fbd468cc33eb7a24eaf511
LV <954777574@qq.com>
2016-07-27 21:12:12 +0800

    Initial Commit

---
+++ SlideViewController/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SlideViewController
+//
+//  Created by LV on 16/7/27.
+//  Copyright © 2016年 LV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
